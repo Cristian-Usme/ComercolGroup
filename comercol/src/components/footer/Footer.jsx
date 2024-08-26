59775fdf602bafb83bdf55e0864b124ae4eda777
Cristian Usme <cristian1213u@gmail.com>
2024-08-26 14:21:54 -0500

    Iniciar Footer

--- comercol/src/components/footer/Footer.jsx
+++ comercol/src/components/footer/Footer.jsx
@@ -1,2 +1,29 @@
-import style from "Footer.module.css"
+import style from "./Footer.module.css"
 
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <div>
+                    <div>
+                        Comercol Group
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer
