b99f092ab065ece0a606a90402def46ce95e18c7
Cristian Usme <cristian1213u@gmail.com>
2024-08-26 16:28:59 -0500

    Avanzar footer 2

--- comercol/src/App.jsx
+++ comercol/src/App.jsx
@@ -1,9 +1,12 @@
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <>
       <Header />
+      <br /><br /><br /><br /><br />
+      <Footer />
     </>
   )
 }
--- comercol/src/components/footer/Footer.jsx
+++ comercol/src/components/footer/Footer.jsx
@@ -1,28 +1,47 @@
-import style from "./Footer.module.css"
+import styles from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <div>
-            <footer>
-                <div>
-                    <div>
-                        Comercol Group
-                    </div>
-                    <div>
 
+        <div className={styles.footer}>
+            <footer className={styles.lblfooter}>
+                <div className={styles.itemsFooter}>
+                    <div className={styles.logoComercol}>
+                        <h1 >Comercol</h1>
+                        <h3 >Group</h3>
                     </div>
-                    <div>
-
+                    <div className={styles.itemContainer}>
+                        <h2 >Oficina</h2>
+                        <ul>
+                            <li className={styles.item}>Cra 33 #33 Norte - 33</li>
+                            <li className={styles.item}>+57 64 4444444</li>
+                            <li className={styles.item}>atencioncliente@comercol.com</li>
+                        </ul>
                     </div>
-                    <div>
-
+                    <div className={styles.itemContainer} >
+                        <h2>Redes</h2>
+                        <ul>
+                            <li className={styles.item}>Facebook</li>
+                            <li className={styles.item}>Instagram</li>
+                            <li className={styles.item}>LinkedIn</li>
+                        </ul>
                     </div>
-                    <div>
-
+                    <div className={styles.itemContainer}>
+                        <h2 >Consultas</h2>
+                        <p className={styles.item}>Para cualquier duda o inquietud <br />
+                            comuniquese a: +57 64 4444444</p>
+                    </div>
+                    <div className={styles.itemContainer}>
+                        <img src="../../../public/vite.svg" alt="" />
                     </div>
                 </div>
+                <div className={styles.privacity}>
+                    <p>Politica de privacidad <br />&copy; Creado por DevIathan Software</p>
+                </div>
+
             </footer>
         </div>
+
     );
 };
 
