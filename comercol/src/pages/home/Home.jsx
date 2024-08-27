1df8d9c99bc856118631d1a0f71e7f4afd94c314
OscarRamos <oscara22a.r@gmail.com>
2024-08-27 13:47:28 -0500

    Iniciar MainShorcuts y ResponsiveCarousel

--- comercol/src/pages/home/Home.jsx
+++ comercol/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import MainParallax from "../../components/home/mainParallax/MainParallax";
+import MainShorcuts from "../../components/home/mainShorcuts/MainShorcuts";
+import ResponsiveCarousel from "../../components/home/slideShorcuts/ResponsiveCarousel";
 
 
 export default function Home() {
     return (
         <div>
             <MainParallax />
+            <MainShorcuts />
+            <ResponsiveCarousel />
         </div>
     )
 }
