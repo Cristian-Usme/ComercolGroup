608c241e4720b4b1a509c06ab892fc11a3b41b89
OscarRamos <oscara22a.r@gmail.com>
2024-08-28 20:15:21 -0500

    Iniciar MainOffer y CarouselOffer 2

--- comercol/src/components/home/mainOffer/CarouselOffer.jsx
+++ comercol/src/components/home/mainOffer/CarouselOffer.jsx
@@ -28,28 +28,28 @@ export default function CarouselOffer() {
                     <div className={style.item}>
                         <img src="https://img.freepik.com/vector-premium/ilustracion-vectorial-concepto-actividad-humana-diaria_1253202-222816.jpg?w=740" alt="" className={style.img} />
                         <div className={style.serviceText}>
-                            <h4>Service</h4>
+                            <h4 className={style.h4}>Service</h4>
                             <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate.</p>
                         </div>
                     </div>
                     <div className={style.item}>
                         <img src="https://img.freepik.com/vector-premium/ilustracion-vectorial-concepto-actividad-humana-diaria_1253202-222816.jpg?w=740" alt="" className={style.img} />
                         <div className={style.serviceText}>
-                            <h4>Service</h4>
+                            <h4 className={style.h4}>Service</h4>
                             <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate.</p>
                         </div>
                     </div>
                     <div className={style.item}>
                         <img src="https://img.freepik.com/vector-premium/ilustracion-vectorial-concepto-actividad-humana-diaria_1253202-222816.jpg?w=740" alt="" className={style.img} />
                         <div className={style.serviceText}>
-                            <h4>Service</h4>
+                            <h4 className={style.h4}>Service</h4>
                             <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate.</p>
                         </div>
                     </div>
                     <div className={style.item}>
                         <img src="https://img.freepik.com/vector-premium/ilustracion-vectorial-concepto-actividad-humana-diaria_1253202-222816.jpg?w=740" alt="" className={style.img} />
                         <div className={style.serviceText}>
-                            <h4>Service</h4>
+                            <h4 className={style.h4}>Service</h4>
                             <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, voluptate.</p>
                         </div>
                     </div>
