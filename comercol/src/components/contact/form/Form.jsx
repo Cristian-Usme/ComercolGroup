2df824ca1271c5dfc2ee51e79bc84ad6b5472b8c
OscarRamos <oscara22a.r@gmail.com>
2024-09-04 15:40:28 -0500

    Avanzar About 2

--- comercol/src/components/contact/form/Form.jsx
+++ comercol/src/components/contact/form/Form.jsx
@@ -17,26 +17,26 @@ const Form = () => {
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <div className={styles.nameAndLastName}>
                             <div>
-                                <label htmlFor="nombre">Nombre</label>
+                                <label htmlFor="nombre" className={`${styles.label} ${styles.labelNames}`}>Nombre</label>
                                 <input type="text" {...register('nombre')} id="nombre" placeholder="Nombre" className={styles.inputNames} />
                             </div>
                             <div>
-                                <label htmlFor="apellido">Apellido</label>
+                                <label htmlFor="apellido" className={`${styles.label} ${styles.labelNames}`}>Apellido</label>
                                 <input type="text" {...register('apellido')} id="apellido" placeholder='Apellido' className={styles.inputNames} />
                             </div>
                         </div>
 
                         <div className={styles.inputsAndLabel}>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" {...register('email')} placeholder='Email' id="email" />
+                            <label htmlFor="email" className={styles.label}>Email</label>
+                            <input type="email" {...register('email')} placeholder='Email' id="email" className={styles.input}/>
                         </div>
                         <div className={styles.inputsAndLabel}>
-                            <label htmlFor="telefono">Teléfono</label>
-                            <input type="tel" {...register('telefono')} placeholder='Numero' id="telefono" />
+                            <label htmlFor="telefono" className={styles.label}>Teléfono</label>
+                            <input type="tel" {...register('telefono')} placeholder='Numero' id="telefono" className={styles.input}/>
                         </div>
                         <div className={styles.inputsAndLabel}>
-                            <label htmlFor="opciones">Opciones</label>
-                            <select id="opciones" multiple {...register('opciones')}>
+                            <label htmlFor="opciones" className={styles.label}>Opciones</label>
+                            <select id="opciones" {...register('opciones')} className={styles.select} >
                                 <option value="opcion1">Opción 1</option>
                                 <option value="opcion2">Opción 2</option>
                                 <option value="opcion3">Opción 3</option>
@@ -44,31 +44,31 @@ const Form = () => {
                             </select>
                         </div>
                         <div className={styles.inputsAndLabel}>
-                            <label htmlFor="mensaje">Mensaje</label>
-                            <textarea {...register('mensaje')} id="mensaje" />
+                            <label htmlFor="mensaje" className={styles.label}>Mensaje</label>
+                            <textarea {...register('mensaje')} id="mensaje" className={styles.textArea}/>
                         </div>
-                        <button type="submit">Enviar</button>
+                        <button type="submit" className={styles.button}>Enviar</button>
                     </form>
                 </div>
 
                 <div>
-                    <h1>A un paso <br /> De comunicarnos </h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe mollitia perferendis, laborum fugit voluptatibus, exercitationem aliquid facilis a maiores libero in omnis, praesentium suscipit earum ipsum assumenda possimus. Nostrum, voluptates.</p>
+                    <h1 className={styles.title}>A un paso <br /> De comunicarnos </h1>
+                    <p className={styles.paragraph}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe mollitia perferendis, laborum fugit voluptatibus, exercitationem aliquid facilis a maiores libero in omnis, praesentium suscipit earum ipsum assumenda possimus. Nostrum, voluptates.</p>
                     <div className={styles.canales}>
                         <div >
-                            <h4>Telefono</h4>
+                            <h4 className={styles.titleRedes}>Telefono</h4>
                             <p>+57 64 4444444</p>
                         </div>
                         <div>
-                            <h4>Email</h4>
+                            <h4 className={styles.titleRedes}>Email</h4>
                             <p>atencioncliente@comercol.com</p>
                         </div>
-                        <div>
-                            <h4>Redes Sociales</h4>
-                            <button><i className="fa-brands fa-facebook-f"></i></button>
-                            <button><i className="fa-brands fa-twitter"></i></button>
-                            <button><i className="fa-brands fa-linkedin-in"></i></button>
-                            <button><i className="fa-brands fa-instagram"></i></button>
+                        <div className={styles.redesSocial}>
+                            <h4 className={styles.titleRedes}>Redes Sociales</h4>
+                            <button className={styles.redes}><i className="fa-brands fa-facebook-f"></i></button>
+                            <button className={styles.redes}><i className="fa-brands fa-twitter"></i></button>
+                            <button className={styles.redes}><i className="fa-brands fa-linkedin-in"></i></button>
+                            <button className={styles.redes}><i className="fa-brands fa-instagram"></i></button>
                         </div>
                     </div>
                 </div>
