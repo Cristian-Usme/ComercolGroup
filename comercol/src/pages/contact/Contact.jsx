ea90d7bb58d0a2e60f88e9a0fc1c83789b10e08b
OscarRamos <oscara22a.r@gmail.com>
2024-09-05 13:37:11 -0500

    Terminar Contact 1.0

--- comercol/src/pages/contact/Contact.jsx
+++ comercol/src/pages/contact/Contact.jsx
@@ -1,10 +1,11 @@
 import Form from "../../components/contact/form/Form";
-
+import VisitUs from "../../components/contact/visitUs/VisitUs";
 
 export default function Contact() {
     return (
         <div>
             <Form />
+            <VisitUs />
         </div>
     )
 }
