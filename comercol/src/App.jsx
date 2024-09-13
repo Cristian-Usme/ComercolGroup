d7ad2c639f8fb9f3f9a683ee9b793b8dd9471fd0
srueda99 <ruedamarinsebastian@gmail.com>
2024-09-13 17:12:35 -0500

    Organizando Contact

--- comercol/src/App.jsx
+++ comercol/src/App.jsx
@@ -1,10 +1,10 @@
-import Header from './components/header/Header'
-// import Footer from './components/footer/Footer'
-import Home from './pages/home/Home'
-import About from './pages/about/About'
-import Products from './pages/products/Products'
-import Contact from './pages/contact/Contact'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Products from "./pages/products/Products";
+import Contact from "./pages/contact/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -17,11 +17,10 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
-        
       </BrowserRouter>
-      {/* <Footer /> */}
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
