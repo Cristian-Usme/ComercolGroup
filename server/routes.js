78bf06bf106221de3a1bb0f67a609e0fd8e89e47
srueda99 <ruedamarinsebastian@gmail.com>
2024-10-06 10:35:57 -0500

    PQR añadido a la ruta

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const tracking = require('./routes/tracking.routes');
+const pqr = require('./routes/pqr.routes');
 
 router.use('/tracking', tracking);
+router.use('/pqr', pqr);
 
 module.exports = router;
