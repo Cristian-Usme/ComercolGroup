1ef8698f414abe43d2a97d63f7a1d4d8b5d6d83f
srueda99 <ruedamarinsebastian@gmail.com>
2024-11-11 22:50:36 -0500

    Chart añadido (básico)

--- comercol/src/components/products/chart/Chart.jsx
+++ comercol/src/components/products/chart/Chart.jsx
@@ -1,9 +1,26 @@
-import style from "./Chart.module.css";
+import styles from "./Chart.module.css";
+import React from "react";
+import CountUp from "react-countup";
 
 export default function Chart() {
   return (
-    <div>
-      <h1>Chart</h1>
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <CountUp start={0} end={3} duration={7} className={styles.cont} />
+          <h2>SEDES</h2>
+        </div>
+
+        <div className={styles.col}>
+          <CountUp start={0} end={1900} duration={7} className={styles.cont} />
+          <h2>PRODUCTOS EXPORTADOS ESTE MES</h2>
+        </div>
+
+        <div className={styles.col}>
+          <CountUp start={0} end={4} duration={7} className={styles.cont} />
+          <h2>PUERTOS DE OPERACIÓN</h2>
+        </div>
+      </div>
     </div>
   );
 }
